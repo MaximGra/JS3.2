9acbae98596a09d8bebfd5240faef7c38b447439
Grachev Maksim <magra85@gmail.com>
2023-06-04 19:02:18 +0300

    fixed

--- chat/task.js
+++ chat/task.js
@@ -1,46 +1,59 @@
-const chatWidget = document.querySelector('.chat-widget');
-const chatWidgetMessages = document.querySelector('.chat-widget__messages');
-const chatWidgetInput = document.querySelector('.chat-widget__input');
+const openChat = document.querySelector('.chat-widget__side-text')
+const container = document.querySelector( '.chat-widget__messages-container' )
+const input = document.getElementById('chat-widget__input')
+const messages = document.querySelector( '.chat-widget__messages' );
 
-const botMessages = [
-    'Добрый день! До свидания!',
-    'Где ваша совесть?',
-    'Кто тут?',
-    'Вы не купили ни одного товара для того, чтобы так с нами разговаривать!',
-    'К сожалению, все операторы сейчас заняты. Не пишите нам больше',
-    'Мы ничего не будем вам продавать!',
-    'Добрый день, мы ещё не проснулись. Позвоните через 10 лет',
-];
-
-const newDate = new Date();
-
-chatWidget.addEventListener('click', function() {
-    chatWidget.classList.add('chat-widget_active');
-});
-
-chatWidget.addEventListener('keydown', function(event) {
-    if (event.code === 'Enter') { 
-       answers();
-    }  
-});
-
-function answers() {    
-
-// Клиент
-chatWidgetMessages.innerHTML += `
-<div class="message message_client">
-    <div class="message__time">${newDate.getHours()}:${newDate.getMinutes()}</div>
-    <div class="message__text">${event.target.value}</div>
-</div>
-`;
-chatWidgetInput.value = '';
-
-// Бот
-const random = Math.floor(Math.random() * botMessages.length);
-
-chatWidgetMessages.innerHTML += `
-<div class="message">
-    <div class="message__time">${newDate.getHours()}:${newDate.getMinutes()}</div>
-    <div class="message__text">${botMessages[random]}</div>
-</div>`;
+function arrayRandElement(arr) {
+    let rand = Math.floor(Math.random() * arr.length);
+    return arr[rand];
 }
+
+function newMessage(text, authorClass) {
+    // let now = new Date().toLocaleTimeString(); // 11:02:48
+    let now = new Date().toLocaleTimeString().slice(0,-3);  // 11:02  
+    messages.innerHTML += `
+    <div class="${authorClass}">
+        <div class="message__time">${now}</div>
+        <div class="message__text">
+        ${text}
+        </div>
+    </div>
+    `;
+    container.scrollTop = container.scrollHeight
+}
+
+newMessage('Добрый день, мы ещё не проснулись. Позвоните через 10 лет', 'message')
+
+openChat.onclick = () => {
+    const parent = openChat.closest('.chat-widget')
+    parent.classList.toggle('chat-widget_active')
+}
+
+input.addEventListener('keydown', (e) => {
+    let idleTime = 0;
+    if (e.key === 'Enter') {
+        if (input.value.trim() != "") {
+            newMessage(input.value.trim(), 'message message_client')
+            input.value = ""
+            newMessage(arrayRandElement(listAnswer), 'message')
+        }
+    }
+});
+
+input.addEventListener('focus', function () {
+    let idleTime = 0;
+    let idleInterval = setInterval(function () {
+        idleTime = idleTime + 1;
+        if (idleTime > 30) { // 30 secondes
+            newMessage(arrayRandElement(listQuestion), 'message')
+            clearInterval(idleInterval)
+        }
+    }, 1000); // 1 seconde
+})
+
+const listAnswer = ['К сожалению все операторы сейчас заняты. Не пишите нам больше!',
+                    'Мы ничего не будем вам продавать!',
+                    'Вы не купили ни одного товара, чтобы с нами разговаривать!']
+
+const listQuestion = ['Молчание затянулось. Вас долго ждать?',
+                    'Где ваша совесть?']
